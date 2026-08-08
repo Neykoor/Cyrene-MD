d03d9446765c840b1a10c0ac50d6e92dc580d749
Drey <huesocalzado9@gmail.com>
2026-08-08 00:33:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -48,29 +48,6 @@ global.moneda = 'dolares';
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
-global.namabot = '⫹⫺  ᴍᴅ'
-global.v = '-'   
-global.eror = "_ubo un error _"
-global.lopr = "🅟"
-global.lolm = "Ⓛ"
-global.dmenut = "✦ ───『"
-global.dmenub = "│➭" 
-global.dmenub2 = "│乂"
-global.dmenuf = "╰━━━━━━━━┈─◂"
-global.cmenut = "⫹⫺ ───『"
-global.cmenuh = "』─── ⬟"
-global.cmenub = "│〆"
-global.cmenuf = "╰━━━━━━━━┈─◂"
-global.cmenua = "\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     "
-global.dashmenu = "✧────···[ *Dashboard* ]···────✧"
-global.htki = '––––––『'
-global.htka = '』––––––'
-global.htjava = "⫹⫺"
-global.comienzo = "• • ◕◕════"
-global.fin = " • •"
-
-//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
-
 // Iᗰᗩᘜ3ᑎᗴՏ ᑎO TOᑕᗩᖇ 📥
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.photoSity = [catalogo]
@@ -95,18 +72,6 @@ ch1: '',
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
 global.APIKeys = {};
-global.MyApiRestBaseUrl = 'https://api.cafirexos.com';
-global.MyApiRestApikey = 'BrunoSobrino';
-global.openai_org_id = 'org-3';
-global.openai_key = 'sk-0';
-global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
-global.lolkeysapi = ['kurumi']; // ['BrunoSobrino_2']
-global.itsrose = ['4b146102c4d500809da9d1ff'];
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 

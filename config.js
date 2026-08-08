79e40db32d186aae6d653554cbfd4bf33b91ab13
Drey <huesocalzado9@gmail.com>
2026-08-08 00:41:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -54,27 +54,6 @@ global.photoSity = [catalogo]
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
-// ᘜᖇᑌᑭOՏ ᗪᗴ ᒪᗩ ᗷOT 🗂️
-global.gp1 = ''
-global.channel2 = ''
-global.md = ''
-global.correo = ''
-global.cn ='';
-
-//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
-
-global.catalogo = fs.readFileSync('./src/catalogo.jpg');
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
-global.ch = {
-ch1: '',
-}
-
-//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
-
-global.APIKeys = {};
-
-//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
-
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)

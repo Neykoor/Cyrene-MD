c14269a60d143a425676474d352030478fdb56d9
Drey <huesocalzado9@gmail.com>
2026-08-20 13:12:28 -0400

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,7 @@ import { sendMenu } from "../plugins/menu";
 import { sendPing } from "../plugins/ping";
 import { sendSticker } from "../plugins/sticker";
 import { sendPinterestImage } from "../plugins/pinterest";
+import { sendPlay } from "../plugins/play";
 
 export async function handleCommand(
   sock: any,
@@ -28,6 +29,10 @@ export async function handleCommand(
 
   if (cmd === "pin" || cmd === "pinterest") {
     await sendPinterestImage(sock, msg, args);
+  }
+
+  if (cmd === "play") {
+    await sendPlay(sock, msg, args.join(" ").trim());
   }
 }
 

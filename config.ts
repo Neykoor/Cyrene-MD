33d83104d0ed09c46e699cb904718fc5d3ddf966
Neykoor <cafecafirexos@gmail.com>
2026-08-20 08:51:39 -0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,19 +1,50 @@
 import fs from "fs";
 import chalk from "chalk";
 
+interface ConfigMedia {
+  menuCover: string;
+  businessThumb: string;
+}
+
 interface ConfigFile {
   prefix: string;
+  pinterestQueries: string[];
+  media: ConfigMedia;
 }
 
 const configFilePath = "./config.json";
 
+const defaultConfig: ConfigFile = {
+  prefix: ".",
+  pinterestQueries: ["cyrene honkai star rail icon", "Japonesas icon"],
+  media: {
+    menuCover: "src/media/menu-cover.jpg",
+    businessThumb: "src/media/business-thumb.jpg",
+  },
+};
+
 if (!fs.existsSync(configFilePath)) {
-  fs.writeFileSync(configFilePath, JSON.stringify({ prefix: "." }, null, 2));
+  fs.writeFileSync(configFilePath, JSON.stringify(defaultConfig, null, 2));
 }
 
-const config: ConfigFile = JSON.parse(fs.readFileSync(configFilePath, "utf-8"));
+const storedConfig: Partial<ConfigFile> = JSON.parse(fs.readFileSync(configFilePath, "utf-8"));
 
-global.prefix = config.prefix || ".";
+const config: ConfigFile = {
+  prefix: storedConfig.prefix || defaultConfig.prefix,
+  pinterestQueries: storedConfig.pinterestQueries?.length
+    ? storedConfig.pinterestQueries
+    : defaultConfig.pinterestQueries,
+  media: {
+    menuCover: storedConfig.media?.menuCover || defaultConfig.media.menuCover,
+    businessThumb: storedConfig.media?.businessThumb || defaultConfig.media.businessThumb,
+  },
+};
+
+fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2));
+
+global.prefix = config.prefix;
+global.pinterestQueries = config.pinterestQueries;
+global.media = config.media;
 
 global.owner = [
   ["526631079388", "Owner", true],
@@ -45,3 +76,5 @@ global.setPrefix = (newPrefix: string): void => {
 export const isOwner = global.isOwner;
 export const setPrefix = global.setPrefix;
 export const allowedPrefixes = global.allowedPrefixes;
+export const pinterestQueries = global.pinterestQueries;
+export const media = global.media;

3e91ed56e6b305cd3a75821f419aa3fa0eef1f80
Neykoor <cafecafirexos@gmail.com>
2026-08-20 12:43:39 -0700

    Delete plugins directory

--- plugins/ping.ts
+++
@@ -1,9 +0,0 @@
-export async function sendPing(sock: any, msg: any): Promise<void> {
-  const chatId: string = msg.key.remoteJid;
-
-  const t0 = Date.now();
-  await sock.sendMessage(chatId, { text: "🏓 Pong!" }, { quoted: msg });
-  const responseMs = (Date.now() - t0).toFixed(2);
-
-  await sock.sendMessage(chatId, { text: `⌁ Velocidad: ${responseMs} ms` }, { quoted: msg });
-}
